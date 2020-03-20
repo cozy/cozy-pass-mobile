f6348c236feac06ec6cb4d782d0bfc81c9919b5a
Patrick Browne <pt.browne@gmail.com>
2020-03-20 10:31:45 +0100

    feat: Open cozy creation page when clicking on register button

--- src/App/Pages/Accounts/HomePage.xaml.cs
+++ src/App/Pages/Accounts/HomePage.xaml.cs
@@ -10,10 +10,15 @@ namespace Bit.App.Pages
     public partial class HomePage : BaseContentPage
     {
         private IMessagingService _messagingService;
+        private IPlatformUtilsService _platformUtilsService;
+        private II18nService _i18nService;
 
         public HomePage()
         {
             _messagingService = ServiceContainer.Resolve<IMessagingService>("messagingService");
+            _platformUtilsService = ServiceContainer.Resolve<IPlatformUtilsService>("platformUtilsService");
+            _i18nService = ServiceContainer.Resolve<II18nService>("i18nService");
+
             _messagingService.Send("showStatusBar", false);
             InitializeComponent();
             _logo.Source = !ThemeManager.UsingLightTheme ? "logo_white.png" : "logo.png";
@@ -43,7 +48,11 @@ namespace Bit.App.Pages
         {
             if(DoOnce())
             {
-                Navigation.PushModalAsync(new NavigationPage(new RegisterPage(this)));
+                #region cozy
+                var lang = _i18nService.Culture.TwoLetterISOLanguageName;
+                _platformUtilsService.LaunchUri($"https://manager.cozycloud.cc/cozy/create?lang={lang}");
+                // Navigation.PushModalAsync(new NavigationPage(new RegisterPage(this)));
+                #endregion
             }
         }
 
