eeb21284930a2f1c3fecd5b64135dacc2cc273dc
Yannick Chiron <yannick.chiron@gmail.com>
2021-11-26 13:41:13 +0100

    feat: rework home page
    Actions buttons are now in the screen's bottom

--- src/App/Pages/Accounts/HomePage.xaml.cs
+++ src/App/Pages/Accounts/HomePage.xaml.cs
@@ -43,7 +43,11 @@ namespace Bit.App.Pages
             _vm.StartRegisterAction = () => Device.BeginInvokeOnMainThread(async () => await StartRegisterAsync());
             _vm.StartSsoLoginAction = () => Device.BeginInvokeOnMainThread(async () => await StartSsoLoginAsync());
             _vm.StartEnvironmentAction = () => Device.BeginInvokeOnMainThread(async () => await StartEnvironmentAsync());
+            // Cozy customization, disable UpdateLogo()
+            // Since we force Inverted theme for HomePage, there is no need to compute Logo based on theme
+            /*
             UpdateLogo();
+            //*/
         }
 
         public async Task DismissRegisterPageAndLogInAsync(string email)
