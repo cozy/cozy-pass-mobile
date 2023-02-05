ac2faf8dd993f6959c634daf53a1c4f759c56c94
Ldoppea <yannick.chiron@gmail.com>
2023-02-05 17:54:19 +0100

    fix: Prevent crash when navigating to Settings page back and forth
    This commit is a cherry pick from a Bitwarden's one that would fix a
    crash when navigating between settings and other pages multiple times
    
    Related commit:
    bitwarden/mobile@7c83d7b37b70739e462695d17b8ede591ad7b9d3

--- src/App/Pages/Settings/SettingsPage/SettingsPageListItemSelector.cs
+++ src/App/Pages/Settings/SettingsPage/SettingsPageListItemSelector.cs
@@ -4,10 +4,15 @@ namespace Bit.App.Pages
 {
     public class SettingsPageListItemSelector : DataTemplateSelector
     {
+        public DataTemplate HeaderTemplate { get; set; }
         public DataTemplate RegularTemplate { get; set; }
 
         protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
         {
+            if (item is SettingsPageHeaderListItem)
+            {
+                return HeaderTemplate;
+            }
             if (item is SettingsPageListItem listItem)
             {
                 return RegularTemplate;
