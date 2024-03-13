d69c3155b5eda7dd4f6f80f0a818b3ed9c8c9ce4
Ldoppea <yannick.chiron@gmail.com>
2024-03-13 23:34:19 +0100

    MODIF 8

---
+++ src/App/Styles/CozyInverted.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Bit.App.Styles
+{
+    public partial class CozyInverted : ResourceDictionary
+    {
+        public CozyInverted()
+        {
+            InitializeComponent();
+        }
+    }
+}
