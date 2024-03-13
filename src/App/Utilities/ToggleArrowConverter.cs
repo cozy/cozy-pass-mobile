e191734aee6937e7ceb4587c32bc1fe89455159a
Ldoppea <yannick.chiron@gmail.com>
2024-03-13 21:53:54 +0100

    MODIF 4

---
+++ src/App/Utilities/ToggleArrowConverter.cs
@@ -0,0 +1,27 @@
+// Cozy customization, ADD description
+//*
+using System;
+using Bit.Core;
+using Xamarin.Forms;
+
+namespace Bit.App.Utilities
+{
+    public class ToggleArrowConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool? isToggled = System.Convert.ToBoolean(value);
+            if (isToggled.HasValue && isToggled.Value == true)
+            {
+                return BitwardenIcons.AngleRight;
+            }
+            return BitwardenIcons.AngleDown;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+//*/
